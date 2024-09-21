8c9ac9aa19f37fa14842bee54be23625059b827e
sharan <sharan.ratnakaram@hotmail.com>
2024-09-21 21:04:34 +0530

    modify the name

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "oidc" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "example-network"
   location            = azurerm_resource_group.oidc.location
   resource_group_name = azurerm_resource_group.oidc.name
