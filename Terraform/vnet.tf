10465d03500ed30510ae3caa4dc9822993680549
sharan <sharan.ratnakaram@hotmail.com>
2024-09-21 21:26:45 +0530

    crate new vnet anf rg tf files

---
+++ Terraform/vnet.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "eastusvnet"
+  location            = azurerm_resource_group.oidc.location
+  resource_group_name = azurerm_resource_group.oidc.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "asiavnet"
+  location            = var.location2
+  resource_group_name = azurerm_resource_group.oidc.name
+  address_space       = ["192.0.0.0/16"]
+}
