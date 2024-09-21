1c77668f509fcd1b5c8e50ccd56567a8f7f3783d
sharan <sharan.ratnakaram@hotmail.com>
2024-09-21 21:05:23 +0530

    added the vnetname

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,14 +9,14 @@ resource "azurerm_resource_group" "oidc" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "example-network"
+  name                = "eastusvnet"
   location            = azurerm_resource_group.oidc.location
   resource_group_name = azurerm_resource_group.oidc.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "asiavnet"
   location            = var.location2
   resource_group_name = azurerm_resource_group.oidc.name
   address_space       = ["192.0.0.0/16"]
